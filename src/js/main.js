7d690b5469e975d731614df6048db86beb5d459b
Dmitry Artemov <art.beliarh@gmail.com>
2018-06-11 17:12:11 +0400

    Радиокнопки

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,13 @@ import './vendor';
 
 const KEY_SPACE = 32;
 
-$(document).on('click', '.checkbox', (e) => {
+$(document).on('click', '.checkbox, .radiobox', (e) => {
 	e.currentTarget.focus();
 });
 
-$(document).on('keydown', '.checkbox', (e) => {
+$(document).on('keydown', '.checkbox, .radiobox', (e) => {
 	if (e.keyCode === KEY_SPACE) {
+		e.preventDefault();
 		e.currentTarget.click();
 	}
 });
