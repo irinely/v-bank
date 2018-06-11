70bc9fe572036afc75346807104cf0e5fafb60f5
Dmitry Artemov <art.beliarh@gmail.com>
2018-06-11 15:19:32 +0400

    Поля и чекбоксы

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import './vendor';
+
+const KEY_SPACE = 32;
+
+$(document).on('click', '.checkbox', (e) => {
+	e.currentTarget.focus();
+});
+
+$(document).on('keydown', '.checkbox', (e) => {
+	if (e.keyCode === KEY_SPACE) {
+		e.currentTarget.click();
+	}
+});
